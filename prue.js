9211e61fb300dee7dcbdd53ef5730e5678b23d86
JosePablo24 <173198@ids.upchiapas.edu.mx>
2020-07-13 14:04:05 -0500

    correccion del programa

--- prue.js
+++ prue.js
@@ -26,9 +26,7 @@ function main() {
     pasa_puntos = false
     pasa_tipo = false
     termina = false
-    pasa_paren_ini = false
     pasa_punto_coma = false
-    n = 1
     if(name == " "){
         console.log("Amigo sabes que tienes que meter mas datos verdad ");
     }else{
@@ -44,118 +42,68 @@ function main() {
         }
         cadena.forEach(element => {
             if( hay_error == false){
-              
                 Terminales.forEach(datas => {
                   if(stack.peek() == datas){                    
                       console.log('actualizar pila');
                       pilaActualizar(datas, reglas, stack,cadena[contador])
                     }                                      
                 });
-                console.log('variable → ', element," Se extrae → ", stack.peek())
+                console.log('Reservadas → ', element," Se extrae → ", stack.peek())
                 if(reservada.test(element)){                  
                   if(contador == 0 && element == 'function'){
                     stack.pop()
-                    if( letras.test(cadena[contador + 1])){
-                      if( cadena[contador + 1] == 'var'){
-                        hay_error = true
-                      }else{
-                        pasa_nombre = true
-                      }     
-                    }else{
-                      hay_error = true
-                    }
+                    if( letras.test(cadena[contador + 1]))
                   }
-                  
-                  if(pasa_var == true && element == 'var'){
-                    stack.pop()                    
+                  if(pasa_var == true && element == 'var'){                    
+                    stack.pop()
+                    pasa_nombre = true
                     pasa_var = false
-                    if( letras.test(cadena[contador + 1])){
-                      if( cadena[contador + 1] == 'var'){
-                        hay_error = true
-                      }else{
-                        pasa_nombre = true
-                      }                      
-                    }else{
-                      hay_error = true
-                    }
                   }
-
-                  if (pasa_tipo == true && ( element == 'string' || element == 'integer' || element == 'boolean' || element == 'real')) {
+                  if (pasa_tipo == true) {                    
                     stack.pop()
                     pasa_tipo = false
                     if(cadena[contador + 1] == ')'){
-                      stack.pop()
                       termina = true
                     }else{
-                      if(cadena[contador + 1 ] == ';'){
-                        pasa_punto_coma = true 
-                      }else{
-                        hay_error == true
-                      }
+                      pasa_punto_coma = true
                     }
                   }
                 }else if (simbolos.test(element)) {
-                  if(element == '(' && pasa_paren_ini == true ){
-                    stack.pop()
-                    pasa_paren_ini = false
-                    if(cadena[contador + 1 ] == 'var'){
+                  if(letras.test(element) == false){
+                    if(contador == 2 && element == '('){                      
+                      stack.pop()
                       pasa_var = true
-                    }else{
-                      hay_error = true
                     }
-                  }
-                  if (pasa_puntos == true && element == ':') {
-                    stack.pop()
-                    pasa_puntos = false
-                    if(cadena[contador + 1] == 'string' || cadena[contador + 1] == 'integer' || cadena[contador + 1] == 'boolean' || cadena[contador + 1] == 'real'){
+                    if (pasa_puntos == true) {                      
+                      stack.pop()
+                      pasa_puntos = false
                       pasa_tipo = true
-                    }else{
-                      hay_error = true
+                    }
+                    if (pasa_punto_coma == true && element == ';') {                      
+                      stack.pop()                      
+                      pasa_nombre = true
+                      pasa_punto_coma = false
+                    }
+                    if(termina == true && element ==')'){                      
+                      stack.pop()
                     }
                   }
-                  if (pasa_punto_coma == true && element == ';') {                      
-                    stack.pop()                                          
-                    pasa_punto_coma = false
-                    if(letras.test(cadena[contador + 1])){
-                      if( cadena[contador + 1] == 'var'){
-                        hay_error = true
-                      }else{
-                        pasa_nombre = true
-                      }
-                    }else{
-                      hay_error = true
-                    }
-                  }
-                  
-                  if(termina == true && element ==')'){                      
-                    stack.pop()
-                  }
-                  
                 }else if(letras.test(element)){
-                  if (pasa_nombre == true && element != 'var'){
-                    if(n == 2){
-                      stack.print()                      
+                  if(stack.peek() != 'var'){                    
+                    if(contador == 1){                      
                       stack.pop()
                       stack.pop()
-                      pasa_nombre = false
-                      if(cadena[contador + 1 ] == ':'){ 
+                    }
+                    if(pasa_nombre == true){
+                        if(element != 'var'){                        
+                        stack.pop()
+                        stack.pop()
+                        pasa_nombre = false
                         pasa_puntos = true
-                      }else{
-                        hay_error = true
-                      }
+                        }else{
+                          hay_error = true
+                        }
                     }
-                    if(n == 1){
-                      stack.pop()
-                      stack.pop()
-                      n += 1
-                      pasa_nombre = false
-                      if(cadena[contador + 1 ] == '('){
-                        pasa_paren_ini = true
-                      }else{
-                        hay_error = true
-                      }
-                    }
-                    
                   }
                 }else{
                     console.log('Hubo algun error');
@@ -169,8 +117,6 @@ function main() {
     if(stack.peek() != '$' || hay_error == true){
       stack.push('¡ERROR!')
       console.log('no saliste correctamente')      
-    }else{
-      console.log('Cadena aceptada')
     }
     stack.print()
 }
